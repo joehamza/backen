c67874b909b827b471d70035740298b4c277233f
joehamza <93442059+joehamza@users.noreply.github.com>
2022-10-22 02:47:46 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+
 <?php
 $request_method=define("URL", str_replace("index.php","",(isset($_SERVER['HTTPS'])? "https" : "http").
 "://".$_SERVER['HTTP_HOST'].$_SERVER["PHP_SELF"]));
@@ -14,26 +15,49 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
   echo $b=json_encode($a);
 
-$formations = json_decode(file_get_contents($b));
 
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+                   
+    function get_data() {
+        $name = $_POST['name'];
+        $file_name =file_get_contents("https://fichierjson.000webhostapp.com/model.json");
+        //$file_name='model'. '.json';
+   
+        if(file_exists("$file_name")) { 
+            $current_data=file_get_contents("$file_name");
+            $array_data=json_decode($current_data, true);
+                               
+            $extra=array(
+                'id' => $_POST['id'],
+                'code' => $_POST['code'],
+                'produit' => $_POST['produit'],
+				'cat' => $_POST['cat'],
+            );
+            $array_data[]=$extra;
+            echo "file exist<br/>";
+            return json_encode($array_data);
+        }
+        else {
+            $datae=array();
+            $datae[]=array(
+                'id' => $_POST['id'],
+                'code' => $_POST['code'],
+                'produit' => $_POST['produit'],
+				'cat' => $_POST['cat'],
+            );
+            echo "file not exist<br/>";
+            return json_encode($datae);   
+        }
+    }
+  
+    $file_name='model'. '.json';
+    if(file_put_contents("$file_name", get_data())) {
+
+        echo 'success';
+    }                
+    else {
+        echo 'There is some error';                
+    }
+}
 ?>
-
-<table border="1">
-    <tr>
-        <td>Id</td>
-        <td>Code produit</td>
-        <td>Produit</td>
-		<td>Categorie</td>
-    </tr>
-    <?php foreach ($formations as $formation) : ?>
-        <tr>
-            
-            <td><?= $formation->id ?></td>
-            <td><?= $formation->code ?></td>
-            <td><?= $formation->produit ?></td>
-			<td><?= $formation->cat ?></td>
-
-        </tr>
-    <?php endforeach; ?>
-</table>
 	
