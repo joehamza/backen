463ea68ee91acc504320f45d4fff5c5d0aa1fc52
joehamza <93442059+joehamza@users.noreply.github.com>
2022-10-22 04:18:44 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,15 @@
 <?php
 $request_method=define("URL", str_replace("index.php","",(isset($_SERVER['HTTPS'])? "https" : "http").
 "://".$_SERVER['HTTP_HOST'].$_SERVER["PHP_SELF"]));
-
-
-
-mysql_connect('localhost','root','');
-    mysql_select_db('oaic');
-	$row=mysql_query("select * from model ");
-	$a=mysql_fetch_array($row);
 	
-//$data=array(
+$data=array(
 
-//'id'=>$b['id'],
-//'code'=>$b['code'],
-//'produit'=>$b['produit'],
-//'cat'=>$b['cat']);
-//}
-$aa[] = $a;
+'id'=>$b['id'],
+'code'=>$b['code'],
+'produit'=>$b['produit'],
+'cat'=>$b['cat']);
+
+$aa[] = $data;
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
   echo $b=json_encode($aa);
