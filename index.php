3d8cebfe2db67593181088502b9a694ee86818bd
joehamza <93442059+joehamza@users.noreply.github.com>
2022-10-29 01:06:52 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,31 +1,14 @@
 <?php
 define("URL", str_replace("index.php","",(isset($_SERVER['HTTPS'])? "https" : "http").
 "://".$_SERVER['HTTP_HOST'].$_SERVER["PHP_SELF"]));
-//if(!empty($_POST["id"])){
-//if(isset($_POST['id'],$_POST['id2'],$_POST['id4'],$_POST['id5'],$_POST['id6'],$_POST['id7'])){
-$data=array(
-'id'=>$_POST['id'],
-'id2'=>$_POST['id2'],
-'id4'=>$_POST['id4'],
-'id5'=>$_POST['id5'],
-'id6'=>$_POST['id6'],
-'id7'=>$_POST['id7']
-);
-$rt= array();
-$rt[]=$data;
-$b=json_encode($rt);
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json");
-  echo"$b";
- file_put_contents('entree.json', $data);
- //file_put_contents('entot.json', $data2);
- //file_put_contents('sortie.json', $data4);
- //file_put_contents('sortot.json', $data5);
- //file_put_contents('entit.json', $data6);
- //file_put_contents('sortit.json', $data7);
 
-//}
- //$sortie = json_decode(file_get_contents("entree.json"));
-  //foreach ($sortie as $sor) :
-      ?>
+ $sortie = json_decode(file_get_contents("https://apibackend3.herokuapp.com/essai/api/webhost2.php"));
+  foreach ($sortie as $sor) :?>
+<=? $sor->id?>
+<=? $sor->id2?>
+<=? $sor->id4?>
+<=? $sor->id5?>
+<=? $sor->id6?>
+<=? $sor->id7?>
+<?php  endforeach; ?>
  
