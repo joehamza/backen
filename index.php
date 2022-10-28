3ce96d38a81f86751eccb348878c0d80a00d464c
joehamza <93442059+joehamza@users.noreply.github.com>
2022-10-29 01:10:51 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 define("URL", str_replace("index.php","",(isset($_SERVER['HTTPS'])? "https" : "http").
 "://".$_SERVER['HTTP_HOST'].$_SERVER["PHP_SELF"]));
 
- $sortie = json_decode(file_get_contents("webhost2.php"));
+ $sortie = json_decode(file_get_contents("essai/api/webhost2.php"));
   foreach ($sortie as $sor) :?>
 <=? $sor->id?>
 <=? $sor->id2?>
