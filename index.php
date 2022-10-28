002861b8ba284f5f31c879e6cd47fab427b260a0
joehamza <93442059+joehamza@users.noreply.github.com>
2022-10-28 20:21:59 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 define("URL", str_replace("index.php","",(isset($_SERVER['HTTPS'])? "https" : "http").
 "://".$_SERVER['HTTP_HOST'].$_SERVER["PHP_SELF"]));
 //if(!empty($_POST["id"])){
-//if(isset($_POST['id'],$_POST['id2'],$_POST['id4'],$_POST['id5'],$_POST['id6'],$_POST['id7'])){
+if(isset($_POST['id'],$_POST['id2'],$_POST['id4'],$_POST['id5'],$_POST['id6'],$_POST['id7'])){
 $data=array(
 'id'=>$_POST['id'],
 'id2'=>$_POST['id2'],
@@ -14,16 +14,17 @@ $data=array(
 
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
-  echo $b=json_encode($data,JSON_UNESCAPED_UNICODE);
- file_put_contents('entree.json', $data);
- file_put_contents('entot.json', $data2);
- file_put_contents('sortie.json', $data4);
- file_put_contents('sortot.json', $data5);
- file_put_contents('entit.json', $data6);
- file_put_contents('sortit.json', $data7);
-//}
- $sortie = json_decode(file_get_contents("entree.json"));
-  foreach ($sortie as $sor) :
+$b=json_encode($data);
+  echo"$b";
+ //file_put_contents('entree.json', $data);
+ //file_put_contents('entot.json', $data2);
+ //file_put_contents('sortie.json', $data4);
+ //file_put_contents('sortot.json', $data5);
+ //file_put_contents('entit.json', $data6);
+ //file_put_contents('sortit.json', $data7);
+
+}
+ //$sortie = json_decode(file_get_contents("entree.json"));
+  //foreach ($sortie as $sor) :
       ?>
- <=?$sor-client ?>;
- <?php  endforeach; ?>
+ 
