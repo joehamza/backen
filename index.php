9d179e5b596c7fcaed558a563e0cc1865e18978d
joehamza <93442059+joehamza@users.noreply.github.com>
2022-10-29 01:32:22 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,11 +4,11 @@ define("URL", str_replace("index.php","",(isset($_SERVER['HTTPS'])? "https" : "h
 
  $sortie = json_decode(file_get_contents("essai/api/webhost2.php"));
   foreach ($sortie as $sor) :?>
-<=? $sor->id?>
-<=? $sor->id2?>
-<=? $sor->id4?>
-<=? $sor->id5?>
-<=? $sor->id6?>
-<=? $sor->id7?>
+<?= $sor->id?>
+<?= $sor->id2?>
+<?= $sor->id4?>
+<?= $sor->id5?>
+<?= $sor->id6?>
+<?= $sor->id7?>
 <?php  endforeach; ?>
  
