09048975801bd68fa5d00242692bbbe34c01e480
joehamza <93442059+joehamza@users.noreply.github.com>
2022-10-28 18:30:11 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 define("URL", str_replace("index.php","",(isset($_SERVER['HTTPS'])? "https" : "http").
 "://".$_SERVER['HTTP_HOST'].$_SERVER["PHP_SELF"]));
 //if(!empty($_POST["id"])){
+if(isset($_POST['id'],$_POST['id2'],$_POST['id4'],$_POST['id5'],$_POST['id6'],$_POST['id7'])){
 $data=array(
 'id'=>$_POST['id'],
 'id2'=>$_POST['id2'],
@@ -10,7 +11,7 @@ $data=array(
 'id6'=>$_POST['id6'],
 'id7'=>$_POST['id7']
 );
-//}
+}
 header("Access-Control-Allow-Origin: *");
 //header("Content-Type: application/json");
   echo $b=json_encode($data);
@@ -20,7 +21,7 @@ header("Access-Control-Allow-Origin: *");
  file_put_contents('sortot.json', $data5);
  file_put_contents('entit.json', $data6);
  file_put_contents('sortit.json', $data7);
-
+}
  $sortie = json_decode(file_get_contents("entree.json"));
   foreach ($sortie as $sor) :
       ?>
