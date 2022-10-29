fb7b5dc52d198778a79271ec8213baba8f802b12
joehamza <93442059+joehamza@users.noreply.github.com>
2022-10-29 18:07:22 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,12 +3,63 @@ define("URL", str_replace("index.php","",(isset($_SERVER['HTTPS'])? "https" : "h
 "://".$_SERVER['HTTP_HOST'].$_SERVER["PHP_SELF"]));
 
  $sortie = json_decode(file_get_contents("essai/api/entree.json"));
-  foreach ($sortie as $sor) :?>
-<?= $sor->client?>
-<?= $sor->c?>
-<?= $sor->q?>
-<?= $sor->wa?>
-<?= $sor->ooo?>
-<?= $sor->mode?>
-<?php  endforeach; ?>
+ <?php
+$sortie = json_decode(file_get_contents("essai/api/entree.json"));
+$sortietot = json_decode(file_get_contents("essai/api/entot.json"));
+$stock = json_decode(file_get_contents("essai/api/sortit.json"));
+ob_start();
+?>
+<center>
+<table border="1" style="border-collapse:collapse;font-size:25px">
+    <?php foreach ($sortietot as $sortot) : ?>
+    <tr><td colspan="7" align="center"><div style="font-size:30px;">*Enlèvements du silo portuaire le <?=$sortot->Date ?></div></td></tr>
+    <?php endforeach; ?>
+    <tr>
+        <th>Organisme</th>
+        <th>Camion</th>
+        <th>Quantité</th>
+        <th>Wagon</th>
+        <th>Quantité</th>
+        <th>Produit</th>
+        <th>Total</th>
+    </tr>
+    <?php foreach ($sortie as $sort) : ?>
+    <?php 
+    $qf=number_format($sort->q,2,',','');
+    $wf=number_format($sort->wag,2,',','');
+    $tot=number_format($sort->ooo,2,',','');?>
+    
+        <tr>
+            <td align="center"><?= $sort->client ?></td>
+            <td align="center"><?= $sort->c ?></td>
+            <td align="center"><?= $qf ?></td>
+            <td align="center"><?= $sort->wa ?></td>
+            <td align="center"><?= $wf ?></td>
+            <td align="center"><?= $sort->mode ?></td>
+            <td align="center"><?= $tot ?></td></tr>
+    <?php endforeach; ?>
+    <?php foreach ($sortietot as $sortot) : ?>
+    <?php 
+    $qf2=number_format($sortot->q2,2,',','');
+    $wf2=number_format($sortot->wag2,2,',','');
+    $tot2=number_format($sortot->ooo2,2,',','');?>
+    <th>Total</th><td align="center"><?= $sortot->c2 ?></td><td align="center"><?= $qf2 ?></td><td align="center"><?= $sortot->wa2 ?></td><td align="center"><?= $wf2 ?></td><td align="center">/</td><td align="center"><?= $tot2 ?></td>
+    <?php endforeach; ?>
+    </table>
+    <br><br>
+    <table border="1" width="30%" style="border-collapse:collapse;font-size:25px">
+    <tr><td colspan="7" align="center"><div style="font-size:30px;">*Stock comptable</div></td></tr>
+    <tr>
+        <th>Produit</th>
+        <th>Stock</th>
+    </tr>
+    <?php foreach ($stock as $st) : ?>
+
+        <tr>
+            <td align="center"><?= $st->produit ?></td>
+            <td align="center"><?= $st->stock ?></td>
+    <?php endforeach; ?>
+    </table>
+    </center>
+    
  
