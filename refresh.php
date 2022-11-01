037ca248360ee9a4e917826803a6ae5447cd6ba5
joehamza <93442059+joehamza@users.noreply.github.com>
2022-11-01 16:46:08 +0100

    Add files via upload

---
+++ refresh.php
@@ -0,0 +1,12 @@
+<?php
+header("refresh:1");
+$entree = json_decode(file_get_contents("essai/api/sortot.json"));
+$entot = json_decode(file_get_contents("essai/api/sortie.json"));
+$entreshif = json_decode(file_get_contents("essai/api/entit.json"));
+$sortie = json_decode(file_get_contents("essai/api/entree.json"));
+$sortietot = json_decode(file_get_contents("essai/api/entot.json"));
+$stock = json_decode(file_get_contents("essai/api/sortit.json"));
+$gbmode = json_decode(file_get_contents("essai/api/gbmode.json"));
+date_default_timezone_set('Africa/Algiers');
+    $heure =date('H:i');
+?>
