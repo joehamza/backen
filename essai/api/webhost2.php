0955455b6b6f7075351a53ef17f8cb6a6800a4e8
joehamza <93442059+joehamza@users.noreply.github.com>
2022-11-05 14:25:44 +0100

    Update webhost2.php

--- essai/api/webhost2.php
+++ essai/api/webhost2.php
@@ -34,6 +34,37 @@ $data8=array(
  // echo"$b";
  //require_once("sortot.txt");
  
+ 
+ 
+ class foo {
+    function __toString() {
+        return __METHOD__;
+    }
+}
+$file = __DIR__."/file_put_contents.txt";
+
+$context = stream_context_create();
+
+try {
+    var_dump(file_put_contents($file, $context));
+} catch (TypeError $e) {
+    echo $e->getMessage(), "\n";
+}
+var_dump(file_put_contents($file, new stdClass));
+var_dump(file_put_contents($file, new foo));
+$fp = fopen($file, "r");
+try {
+    var_dump(file_put_contents($file, "string", 0, $fp));
+} catch (TypeError $e) {
+    echo $e->getMessage(), "\n";
+}
+
+@unlink($file);
+
+echo "Done\n";
+ 
+ 
+ 
  file_put_contents('entree.txt', $data);
 file_put_contents('entot.txt', $data2);
  file_put_contents('sortie.txt', $data4);
