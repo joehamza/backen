1fefc1e05a9d89135638367983903239f18f17f2
joehamza <93442059+joehamza@users.noreply.github.com>
2022-11-05 16:06:50 +0100

    Update webhost2.php

--- essai/api/webhost2.php
+++ essai/api/webhost2.php
@@ -34,7 +34,7 @@ $data8=array(
  // echo"$b";
  //require_once("sortot.txt");
  
-$fichier = fopen('sortot.txt', 'c+b');
+$fichier = fopen('sortot.txt', 'r');
             fwrite($fichier, 'Un premier texte dans mon fichier'); 
  
  file_put_contents('entree.txt', $data);
