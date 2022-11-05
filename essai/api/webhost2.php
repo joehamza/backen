b9ffef19f29616474183d5c3c6657939af589829
joehamza <93442059+joehamza@users.noreply.github.com>
2022-11-05 16:19:08 +0100

    Update webhost2.php

--- essai/api/webhost2.php
+++ essai/api/webhost2.php
@@ -34,7 +34,7 @@ $data8=array(
  // echo"$b";
  //require_once("sortot.txt");
  
-$fichier = fopen('https://joehamza.github.io/backen/essai/api/sortot.txt', 'w');
+$fichier = fopen('https://joehamza.github.io/backen/essai/api/sortot.txt', 'r');
             fwrite($fichier, 'Un premier texte dans mon fichier'); 
  
  file_put_contents('entree.txt', $data);
